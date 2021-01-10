7fef2f83dea30bfff7b19e9377a0ea836a61d63f
BFFV <bffarias@uc.cl>
2021-01-10 00:13:36 -0300

    :art: :sparkles: Added assets & coded basic functions.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public GameObject piece;
+
+    // Positions and team for each piece
+    private GameObject[,] positions = new GameObject[9, 9];
+    private GameObject[] playerWhite = new GameObject[16];
+    private GameObject[] playerBlack = new GameObject[16];
+    private string currentPlayer = "white";
+    private bool gameOver = false;
+
+    void Start() {
+        playerWhite = new GameObject[] {
+            Create("wNinja", 4, 7)
+        };
+        playerBlack = new GameObject[] {
+            Create("bNinja", 4, 1)
+        };
+
+        // Set all piece positions on the board
+        for (int i = 0; i < playerBlack.Length; i++) {
+            SetPosition(playerWhite[i]);
+            SetPosition(playerBlack[i]);
+        }
+    }
+
+    public GameObject Create(string name, int x, int y) {
+        GameObject obj = Instantiate(piece, new Vector3(0, 0, -1), Quaternion.identity);
+        Piece gamePiece = obj.GetComponent<Piece>();
+        gamePiece.name = name;
+        gamePiece.SetXBoard(x);
+        gamePiece.SetYBoard(y);
+        gamePiece.Activate();
+        return obj;
+    }
+
+    public void SetPosition(GameObject obj) {
+        Piece gamePiece = obj.GetComponent<Piece>();
+        positions[gamePiece.GetXBoard(), gamePiece.GetYBoard()] = obj;
+    }
+
+    void Update() {
+    }
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    // References
+    public GameObject controller;
+    public GameObject moveTile;
+
+    // Positions
+    private int xBoard = -1;
+    private int yBoard = -1;
+
+    // Player color
+    private string player;
+
+    // Sprite references
+    public Sprite wNinja;
+    public Sprite bNinja;
+
+    public void Activate() {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        // Adjust the transform
+        SetCoords();
+
+        switch (this.name) {
+            case "wNinja": this.GetComponent<SpriteRenderer>().sprite = wNinja; break;
+            case "bNinja": this.GetComponent<SpriteRenderer>().sprite = bNinja; break;
+        }
+    }
+
+    public void SetCoords() {
+        float x = xBoard;
+        float y = yBoard;
+        x -= 4;
+        y -= 4;
+        this.transform.position = new Vector3(x, y, -1);
+    }
+
+    public int GetXBoard() {
+        return xBoard;
+    }
+
+    public int GetYBoard() {
+        return yBoard;
+    }
+
+    public void SetXBoard(int x) {
+        xBoard = x;
+    }
+
+    public void SetYBoard(int y) {
+        yBoard = y;
+    }
+}
